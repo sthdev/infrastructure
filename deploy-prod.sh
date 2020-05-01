48c6dc0ed6701e4f893e3ec38a9ef965cc08983b
sthdev <sthdev@posteo.de>
2020-05-01 14:23:58 +0200

    set up portainer with admin password

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventories/production.yml playbook.yml --ask-become-pass
+ansible-playbook -i inventories/production.yml playbook.yml --ask-become-pass --ask-vault-pass

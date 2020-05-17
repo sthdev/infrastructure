085075e6d27103a2105049b442ba6132fac50743
sthdev <sthdev@posteo.de>
2020-05-17 16:03:22 +0000

    removed --ask-pass parameter

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventories/test.yml playbook.yml --ask-become-pass --ask-vault-pass --ask-pass
+ansible-playbook -i inventories/test.yml playbook.yml --ask-become-pass --ask-vault-pass

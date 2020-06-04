061b57ab1dee38b14d7e49cb1dc9ed12f71c652b
sthdev <sthdev@posteo.de>
2020-06-04 15:35:08 +0000

    changed url

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,4 +2,4 @@
 
 # Call with deploy-prod <playbook-X.yml>
 ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventories/production.yml $1 --ask-become-pass --ask-vault-pass
+ansible-playbook -i inventories/production.yml $1 --ask-become-pass --ask-vault-pass --ask-pass
